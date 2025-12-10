01ef089c7d3f32ee53b8084ff159d3140cc81202
adriantwill <142526523+adriantwill@users.noreply.github.com>
2025-12-10 16:46:39 -0500

    cleaned up cloudflare files

--- .open-next/server-functions/default/.next/server/app/api/generate-questions/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/generate-questions/route.js")
-R.c("server/chunks/[root-of-the-server]__03fe02e0._.js")
-R.c("server/chunks/node_modules_0753c131._.js")
-R.c("server/chunks/[root-of-the-server]__b2da729a._.js")
-R.c("server/chunks/_next-internal_server_app_api_generate-questions_route_actions_7a81e194.js")
-R.m(4921)
-module.exports=R.m(4921).exports
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eTLKRiXUdZUoT2pwr7KtXoNb7BeT1DUkPVO7pj1bKp0=\"\n}"
